9d46fc54db4a4f26b8924d08c779baa338babb3d
Yash Saini <ysaini@paymentus.com>
2021-02-15 14:44:50 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,6 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+export * from './server';
+export * from './app';
