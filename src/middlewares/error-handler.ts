914cba0579d5e9f9392bed2bc9ac0f9e2a6ed342
Yash Saini <ysaini@paymentus.com>
2021-02-18 10:40:05 +0530

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -8,7 +8,7 @@ const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    return res.status(400).send({errors: err.serializeErrors()});
+    return res.status(err.statusCode).send({errors: err.serializeErrors()});
   }
 
   return res.status(400).send({
